7a9f837dcccae9798f3f63d1747bd169dcece002
Ben Smith <benjiiman12@gmail.com>
2017-08-06 11:02:37 +1200

    changed to POST method

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php 
-header("Content-Type: text/plain;charset=utf-8");
+//header("Content-Type: text/plain;charset=utf-8");
 $http_origin = $_SERVER['HTTP_ORIGIN'];
 
 if ($http_origin == "http://127.0.0.1:51268")
-    { 
-    header('Access-Control-Allow-Origin: *');
-    }
+{ 
+	header('Access-Control-Allow-Origin: *');
+}
 
 $name = $_POST['name'];
 
 if ($name == "ben"){
-	echo "name is ben!";
+	echo "name is ben! " . $name;
 } else {
-	echo "didn't work :(";
+	echo "didn't work :( " . $name;
 }
 
 ?>
