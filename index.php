9ebbab79e2e35af769b0a548edd8f26c1a27df71
Ben Smith <benjiiman12@gmail.com>
2017-08-06 10:32:32 +1200

    trying post method

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
-<?php
+<?php 
+$http_origin = $_SERVER['HTTP_ORIGIN'];
 
-echo "dfgdfgsfg";
+ if ($http_origin == "http://127.0.0.1:51268")
+    { 
+    header('Access-Control-Allow-Origin: *');
+    }
 
+$name = $_GET['name'];
+
+if ($name == "ben"){
+	echo "name is ben!";
+} else {
+	echo "didn't work :(";
+}
+// header("Content-Type: text/plain;charset=utf-8");
 ?>
